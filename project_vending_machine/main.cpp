c70066bbc51ac661dba970f6c4b39dbe97bc0045
Thomas Brandon <tsbrandon1010@gmail.com>
2023-03-07 21:48:47 -0500

    3/7

--- project_vending_machine/main.cpp
+++ project_vending_machine/main.cpp
@@ -8,12 +8,18 @@
 
 
     -- Service Mode
+    
+    TO DO:
+    
     * help - list commands
-    * status - print the till, print number of cans and cups
     * add cans / cups
     * add / remove money
     * exit
     * lock with password
+
+    DONE:
+    
+    * status - print the till, print number of cans and cups
 
     -- Normal Mode
     * help - list commands
@@ -27,22 +33,37 @@
 
 using namespace std;
 
+string stringUpper(string aString) {
+    for (int i = 0; i < aString.size(); i++) {
+        aString[i] = toupper(aString[i]);
+    }
+    return aString;
+}
+
+string stringLower(string aString) {
+    for (int i = 0; i < aString.size(); i++) {
+        aString[i] = tolower(aString[i]);
+    }
+    return aString;
+}
+
 class Currency {
 public:
     string name;
+    string printableName;
     int count;
     float value;
 
-//public:
-    //void setCount{}
     Currency() {
         this->name = "";
+        this->printableName = "";
         this->count = 0;
         this->value = 0;
     }
     
-    void setValues(string pName, int pCount, float pValue) {
+    void setValues(string pName, string pPrintableName , int pCount, float pValue) {
         this->name = pName;
+        this->printableName = pPrintableName;
         this->count = pCount;
         this->value = pValue;
     }
@@ -66,6 +87,7 @@ public:
         this->count = pCount;
         this->price = pPrice;
     }
+
 };
 
 class VendingMachineTill {
@@ -82,11 +104,11 @@ private:
 public:
 
     VendingMachineTill() {
-        fiveDollar.setValues("Five Dollar Bills", 2, 5.0);
-        dollar.setValues("One Dollar Bills", 2, 1.0);
-        quarter.setValues("Quarters", 1, 0.25);
-        dime.setValues("Dimes", 0, 0.10);
-        nickel.setValues("Nickels", 0, 0.05);
+        fiveDollar.setValues("Five", "Five Dollar Bills", 2, 5.0);
+        dollar.setValues("One", "One Dollar Bills", 2, 1.0);
+        quarter.setValues("Quarter", "Quarters", 1, 0.25);
+        dime.setValues("Dime", "Dimes", 0, 0.10);
+        nickel.setValues("Nickel", "Nickels", 0, 0.05);
         this->calculateAmountDeposited();
     }
 
@@ -107,17 +129,213 @@ public:
         this->calculateAmountDeposited();
         cout << "Amount Deposited = " << amountDeposited << endl;
         for (int i = 0; i < 5; i++) {
-            cout << "total " << tender[i]->name << " = " << tender[i]->count << endl;
+            cout << "total " << this->tender[i]->printableName << " = " << 
+            tender[i]->count << endl;
         }
     }
 
-    
+    int findItem(string pName) { // returns the index of an item's name
+        
+        pName = stringLower(pName);
+        pName[0] = toupper(pName[0]);
+
+        for (int i = 0; i < TILL_SIZE; i++) {
+            if (this->tender[i]->name == pName) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    void printCount(int itemIndex) {
+        cout << "Current quantity of " << this->tender[itemIndex]->printableName
+        << ": "<< this->tender[itemIndex]->count << endl; 
+    }
+
+    void add(string itemType, string denomination, int quantity) {
+        if (quantity < 0) {
+            cout << "Cannot add a number less than zero" << endl;
+            return;
+        }
+        
+        itemType = stringLower(itemType);
+        if (itemType == "coins" || itemType == "coin" || itemType == "bills" || itemType == "bill") {
+            int itemIndex = findItem(denomination);
+            
+            if (itemIndex == -1) {
+                cout << "Invalid denomination was entered" << endl;
+                return;
+            }
+            
+            else {
+                this->tender[itemIndex]->count += quantity;
+                printCount(itemIndex);
+                return;
+            }
+        }
+        
+        else {
+            cout << "The type [Coins|Bills] was not correct" << endl;
+            return;
+        }
+    }
+
+    void add(string itemType, int denomination, int quantity) {
+        if (quantity < 0) {
+            cout << "Cannot add a number less than zero" << endl;
+            return;
+        }
+        
+        // the breaks in this switch statement are probably redundent
+
+        itemType = stringLower(itemType);
+        if (itemType == "coins" || itemType == "coin") {
+            
+            switch (denomination)
+            {
+            case 5:
+                this->tender[4]->count += quantity;
+                printCount(4);
+                return;
+                break;
+            case 10:
+                this->tender[3]->count += quantity;
+                printCount(3);
+                return;
+                break;
+            case 25:
+                this->tender[2]->count += quantity;
+                printCount(2);
+                return;
+                break; 
+            default:
+                cout << "An invalid denomination was entered" << endl;
+                return;
+                break;
+            }
+        }
+        
+        if (itemType == "bills" || itemType == "bill") {
+            switch (denomination)
+            {
+                case 5:
+                    this->tender[0]->count += quantity;
+                    printCount(0);
+                    return;
+                    break;
+                case 1:
+                    this->tender[1]->count += quantity;
+                    printCount(1);
+                    return;
+                    break;
+                default:
+                    cout << "An invalid denomination was entered" << endl;
+                    return;
+                    break;
+            }
+        }
+
+        else {
+            cout << "The type [Coins|Bills] was not correct" << endl;
+            return;
+        }
+    }
+
+    void remove(string itemType, string denomination, int quantity) {
+        if (quantity < 0) {
+            cout << "Cannot remove a number less than zero" << endl;
+            return;
+        }
+        
+        itemType = stringLower(itemType);
+        if (itemType == "coins" || itemType == "coin" || itemType == "bills" || itemType == "bill") {
+            int itemIndex = findItem(denomination);
+            
+            if (itemIndex == -1) {
+                cout << "Invalid denomination was entered" << endl;
+                return;
+            }
+            else {
+                
+                if (this->tender[itemIndex]->count <= quantity) { this->tender[itemIndex]->count = 0; } 
+                else { this->tender[itemIndex]->count -= quantity; }
+                printCount(itemIndex);
+                return;
+            }
+        }
+        else {
+            cout << "The type [Coins|Bills] was not correct" << endl;
+            return;
+        }
+    }
+
+    void remove(string itemType, int denomination, int quantity) {
+        if (quantity < 0) {
+            cout << "Cannot add a number less than zero" << endl;
+            return;
+        }
+        
+        itemType = stringLower(itemType);
+        if (itemType == "coins" || itemType == "coin") {
+            
+            if (denomination == 5) {
+                if (this->tender[4]->count <= quantity) { this->tender[4]->count = 0; }
+                else { this->tender[4]->count -= quantity; }
+                printCount(4);
+                return;
+            }
+            if (denomination == 10) {
+                if (this->tender[3]->count <= quantity) { this->tender[3]->count = 0; }
+                else { this->tender[3]->count -= quantity; }
+                printCount(3);
+                return;
+            }
+            if (denomination == 25) {
+                if (this->tender[2]->count <= quantity) { this->tender[2]->count = 0; }
+                else { this->tender[2]->count -= quantity; }
+                printCount(2);
+                return;
+            }
+            else {
+                cout << "An invalid denomination was entered" << endl;
+                return;
+            }
+        }
+        
+        if (itemType == "bills" || itemType == "bill") {
+            
+            if (denomination == 5) {
+                if (this->tender[0]->count <= quantity) { this->tender[0]->count = 0; }
+                else { this->tender[0]->count -= quantity; }
+                printCount(0);
+                return;
+            }
+            if (denomination == 1) {
+                if (this->tender[1]->count <= quantity) { this->tender[1]->count = 0; }
+                else { this->tender[1]->count -= quantity; }
+                printCount(1);
+                return;
+            }
+            else {
+                cout << "An invalid denomination was entered" << endl;
+                return;
+            }
+        }
+
+        else {
+            cout << "The type [Coins|Bills] was not correct" << endl;
+            return;
+        }
+    }    
+
 };
 
 class VendingMachineInventory {
 public:
 
     const float DRINK_COST = .75;
+    static const int INVENTORY_SIZE = 6;
     Item coke;
     Item pepsi;
     Item RC;
@@ -125,7 +343,7 @@ public:
     Item faygo;
     Item cup;
 
-    Item* inventory[6] = {&coke, &pepsi, &RC, &jolt, &faygo, &cup}; 
+    Item* inventory[INVENTORY_SIZE] = {&coke, &pepsi, &RC, &jolt, &faygo, &cup}; 
 
     VendingMachineInventory() {
         coke.setValues("Coke", 0, DRINK_COST);
@@ -137,11 +355,74 @@ public:
         // cups are free
         cup.setValues("Cups", 0, 0);
     }
+    
     void printInventory() {
-        for(int i = 0; i < inventory.size(); i++){
-            
+        for(int i = 0; i < INVENTORY_SIZE; i++){
+            cout << "total " << this->inventory[i]->name << " = " 
+            << this->inventory[i]->count << endl;
         }
     }
+
+    void printCount(int itemIndex) {
+        cout << "Current quantity of " << this->inventory[itemIndex]->name
+        << ": "<< this->inventory[itemIndex]->count << endl; 
+    }
+
+    int findItem(string pName) {
+        if (stringUpper(pName) == "RC") {
+            return 2;
+        }
+        else {
+            pName = stringLower(pName);
+            pName[0] = toupper(pName[0]);
+            for(int i = 0; i < INVENTORY_SIZE; i++) {
+                if (this->inventory[i]->name == pName) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    void add(string itemType, string denomination, int quantity) {
+        if (quantity < 0) {
+            cout << "Cannot add a number less than zero" << endl;
+            return;
+        }
+        
+
+
+        
+    }
+
+    void remove(string itemType, string denomination, int quantity) {
+        if (quantity < 0) {
+            cout << "Cannot remove a number less than zero" << endl;
+            return;
+        }
+        
+        itemType = stringLower(itemType);
+        if (itemType == "coins" || itemType == "coin" || itemType == "bills" || itemType == "bill") {
+            int itemIndex = findItem(denomination);
+            
+            if (itemIndex == -1) {
+                cout << "Invalid denomination was entered" << endl;
+                return;
+            }
+            else {
+                
+                if (this->tender[itemIndex]->count <= quantity) { this->tender[itemIndex]->count = 0; } 
+                else { this->tender[itemIndex]->count -= quantity; }
+                printCount(itemIndex);
+                return;
+            }
+        }
+        else {
+            cout << "The type [Coins|Bills] was not correct" << endl;
+            return;
+        }
+    }
+
 };
 
 class VendingMachine {
@@ -158,9 +439,6 @@ public:
     VendingMachineInventory inventory;    
     VendingMachineTill till;
 
-
-
-
     void setWorkingMode(bool workMode) {
         this->workingMode = workMode;
     }
@@ -174,15 +452,18 @@ public:
     
     // service mode
     void status() { // this command prints out the ammount deposited, the machine's till, and the number of pop/cups
-
+        this->till.printTill();
+        this->inventory.printInventory();
     }
+
+    void add() {} // the vending machine has to call the correct corresponding add function (from the till or the inventory)
 };
 
 int main() {
 
-    VendingMachineTill myTill;
+    VendingMachineTill till;
 
-    myTill.printTill();
+    till.remove("bills", 5, 10);
 
     system("pause");
     return 0;
